ccc18046a8092a32dbcae600114135d469e6e9c1
YuriSilenok <yuri.silenok@gmail.com>
2025-07-25 15:59:06 +0300

    Удаление лишнего файла

--- controllers/user.py
+++
@@ -1,8 +0,0 @@
-import functools
-
-
-def validate(func):
-    """Валидирует пользователя и передает модель пользователя в функцию"""
-    
-    @functools.wraps(func)
-    async def wrapper(*args, **qwargs):
